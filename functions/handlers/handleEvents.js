44414c855324bdcfb5bc1999dcffccc1085f1e62
jairusu <jairusudanieru@gmail.com>
2023-05-31 11:31:14 +0800

    landdroid

--- functions/handlers/handleEvents.js
+++ functions/handlers/handleEvents.js
@@ -11,8 +11,11 @@ module.exports = (client) => {
                 case "client":
                     for (const file of eventFiles) {
                         const event = require(`../../events/${folder}/${file}`);
-                        if (event.once) client.once(event.name, (...args) => event.execute(...args, client));
-                        else client.on(event.name, (...args) => event.execute(...args, client))
+                        if (event.once) {
+                            client.once(event.name, (...args) => event.execute(...args, client));
+                        } else {
+                            client.on(event.name, (...args) => event.execute(...args, client))
+                        }
                     }
                     break;
 
