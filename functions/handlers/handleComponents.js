4ca4fed1f56b9de030345d3fa0b16844026a7fc8
jairusu <jairusudanieru@gmail.com>
2023-08-03 02:50:35 +0800

    landdroid

--- functions/handlers/handleComponents.js
+++ functions/handlers/handleComponents.js
@@ -26,23 +26,10 @@ module.exports = (client) => {
                         buttons.set(ticket.data.name, ticket);
                     }
                     break;
-                case "pingroles":
-                    for (const file of componentFiles) {
-                        const ping = require(`../../components/${folder}/${file}`)
-                        buttons.set(ping.data.name, ping);
-                    }
-                    break;
                 case "startpages":
                     for (const file of componentFiles) {
                         const str = require(`../../components/${folder}/${file}`)
                         buttons.set(str.data.name, str);
-                    }
-                    break;
-
-                case "customvc":
-                    for (const file of componentFiles) {
-                        const vc = require(`../../components/${folder}/${file}`)
-                        buttons.set(vc.data.name, vc);
                     }
                     break;
                 default:
